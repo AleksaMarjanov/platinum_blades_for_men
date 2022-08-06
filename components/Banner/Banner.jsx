a4ec5cb9f93b770301a95e74376530d27d59615b
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-05 19:44:41 -0500

    ..

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -13,8 +13,8 @@ const Banner = ({ name, childStyles, parentStyle }) => {
   return(
     <>
     <div className={`relative w-full flex items-center overflow-hidden ${parentStyle}`}> 
-      <p className={`font-bold z-10 text-5xl text-white font-poppins leading-70 absolute  ${childStyles}`}>{name}</p>
-      <Image className={`relative  ${theme === 'light' ? 'opacity-100' : 'opacity-60'}`} src={images.barbertools} objectFit="contain" alt="barber tools hero"/>
+      <p className={`font-bold text-5xl dark:text-white text-dark font-poppins leading-70 absolute ${childStyles}`}>{name}</p>
+      <Image className={`relative  ${theme === 'light' ? 'opacity-70' : 'opacity-60'}`} src={images.barbertools} objectFit="contain" alt="barber tools hero"/>
       <Button  btnName="Book" classStyles="absolute bottom-10 sm:pt-2 sm:pt-2 md:bottom-3 lg:bottom-3 rounded-full items-center justify-center cursor-pointer " handleClick={() => router.push('http://platinumbladesformen.booksy.com/')} />
     </div>
    
--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import { client, urlFor } from '../../pages/client.js'
 
 const Testimonials = () => {
+  const handleClick = ( index ) => {
+    setCurrentIndex(index)
+  }
 
+  const { theme } = useTheme();
   const [testimonials, setTestimonials] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
 
@@ -21,10 +26,10 @@ const Testimonials = () => {
   const test = testimonials[currentIndex];
   
   return (
-    <>
+    <div className="flex items-center w-full flex-col">
       {testimonials.length && (
         <>
-        <div className="w-[60%] min-h-[320px] flex flex-col p-5 rounded-2xl">
+        <div className={`w-[60%] min-h-[320px] mt-16  flex flex-col p-5 rounded-2xl --tw-shadow-color: #fff ${theme === 'dark' ? 'bg-nft-dark' : 'bg-white'}`}>
           <Image src={`${urlFor(test.imgurl)}`} 
           layout="fixed"
           width={100}
@@ -32,20 +37,41 @@ const Testimonials = () => {
           className="rounded-full"
           objectFit="cover"
           alt="testimonial" />
-          <div>
-            <p>{test.feedback}</p>
+          <div className="flex flex-col justify-center flex-start flex-1 h-full">
+            <p className='text-xl lg:text-2xl'>{test.feedback}</p>
             <div>
-              <h4>{test.name}</h4>
+              <h4 className="font-bold">{test.name}</h4>
             </div>
           </div>
         </div>
 
         <div className="">
-
+          <div 
+          className=""
+          onClick={() => 
+          handleClick(
+            currentIndex === 0
+            ? testimonials.length - 1
+            : currentIndex - 1
+          )}
+          >
+            <HiChevronLeft />
+          </div>
+          <div
+          className=""
+          onClick={() => 
+          handleClick(
+            currentIndex === testimonials.length - 1
+            ? 0 
+            : currentIndex + 1 
+          )}
+          >
+            <HiChevronRight  />
+          </div>
         </div>
         </>
       )}
-    </>
+    </div>
   )
 }
 
