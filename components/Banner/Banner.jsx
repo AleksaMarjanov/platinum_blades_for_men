f0726e6a4fe11ad3f4ad353ce713b6355f8fa405
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-08 20:08:18 -0500

    fixed text bug on hero image

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -12,9 +12,9 @@ const Banner = ({ name, childStyles, parentStyle, isMobile }) => {
   
   return(
     <>
-    <div className={`relative w-full flex items-center overflow-hidden ${parentStyle}`}> 
-      <p className={`font-bold text-5xl dark:text-white text-white font-poppins leading-70 absolute ${isMobile ? 'z-0' : 'z-10'} ${childStyles}`}>{name}</p>
-      <Image className={`relative  ${theme === 'light' ? 'opacity-100' : 'opacity-100'}`} src={images.barbertools} objectFit="contain" alt="barber tools hero"/>
+    <div className={`relative  z-0 w-full flex items-center overflow-hidden ${parentStyle}`}> 
+      <p className={`font-bold text-5xl dark:text-white text-white font-poppins leading-70 absolute w-full flex justify-center items-center z-10 ${childStyles}`}>{name}</p>
+      <Image className={`relative  ${theme === 'light' ? 'opacity-100' : 'opacity-70'}`} src={images.barbertools} objectFit="contain" alt="barber tools hero"/>
       <Button  btnName="Book" classStyles="absolute bottom-5 xs:m-2 sm:pt-2 sm:mb-5 md:bottom-3 lg:bottom-3 rounded-full items-center justify-center cursor-pointer " handleClick={() => router.push('http://platinumbladesformen.booksy.com/')} />
     </div>
    
