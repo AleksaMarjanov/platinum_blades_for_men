82788c0614eb48b8868493c2c1e33a6f6a1e9dff
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-10 17:53:14 -0500

    ..

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-center text-3xl">Footer</div>
+    <div className="">
+      <div className="w-full pb-0 flex flex-col justify-end items-end">
+            <p className="text-xs text-left line lg:text-sm">@{new Date().getFullYear()} Aleksa</p>
+            <p className="text-xs text-left line lg:text-sm mb-5">All rights reserved</p>
+          </div>
+    </div>
   )
 }
 
--- components/Wrapper/AppWrap.js
+++ components/Wrapper/AppWrap.js
@@ -7,11 +7,6 @@ const AppWrap = (Component, idName, classNames) =>
       <div id={idName} className={`w-full min-h-screen flex flex-row ${classNames}`}>
         <div className="flex-1 w-full flex-col justify-center items-center">
           <Component />
-
-          <div className="w-full pb-0 flex flex-col justify-end items-end">
-            <p className="text-xs text-left line lg:text-sm">@{new Date().getFullYear()} Aleksa</p>
-            <p className="text-xs text-left line lg:text-sm mb-5">All rights reserved</p>
-          </div>
         </div>
         <NavigationDots active={idName} />
       </div>
