354cd973e687c50ac703dbf8f317c9335e979b7f
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-09 20:41:43 -0500

    Add About section

--- assets/index.js
+++ assets/index.js
@@ -3,5 +3,6 @@ import shop from './1.jpg'
 import menu from './menu.png'
 import barbertools from './barbertools.webp'
 import cross from './cross.png'
+import about1 from './about1.jpg'
 
-export default { logo, shop, menu, cross, barbertools }
+export default { logo, shop, menu, cross, barbertools, about1 }
--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
+import images from '../../assets'
+import AppWrap from './../Wrapper/AppWrap';
+import MotionWrap from '../Wrapper/MotionWrap';
 
 const About = () => {
   return (
-    <div id="about" className="font-poppins text-4xl flex justify-center items-center">About</div>
+    <>
+    <div id="about" className="font-poppins text-4xl flex justify-start">
+    AT PLATINUM BLADES, WE<br/> BELIEVE THAT, AT ITS BEST,<br/>
+      A BARBERSHOP IS A <br/>DEPENDABLE, TRUSTED<br/>NEIGHBORHOOD INSTITUTION.
+    </div>
+    <div>
+        <Image src={images.about1} alt="work hairstyle" width={400} height={1000} objectFit="contain" />
+    </div>
+    </>
   )
 }
 
-export default About
+export default AppWrap(
+  MotionWrap(
+    About,
+    "about"
+  ))
