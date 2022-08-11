a4afa5ec93ad338f8d02a1932f2b499c127dc2b1
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-08-11 22:25:35 +0000

    ..

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import SocialMedia from '../SocialMedia'
 
 const Footer = () => {
   return (
-    <div className="">
+    <div className="bg-black">
       <div className="w-full pb-0 flex flex-col justify-end items-end">
             <p className="text-xs text-left line lg:text-sm">@{new Date().getFullYear()} Aleksa</p>
             <p className="text-xs text-left line lg:text-sm mb-5">All rights reserved</p>
           </div>
+          <SocialMedia />
     </div>
   )
 }
--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BsTwitter, BsLinkedin, BsGithub } from "react-icons/bs";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
 import { useTheme } from 'next-themes';
 
 const SocialMedia = () => {
   const { theme } = useTheme();
    
   return (
-    <div className="flex justify-center items-center flex-col p-4">
+    <div className="flex justify-center items-center flex-row">
       <div className="w-[40px] h-[40px] rounded-full m-1 border-solid border-slate-400">
         <a href="https://twitter.com/beli1337" target="_blank" rel="noreferrer">
-          <BsTwitter />
+          <BsInstagram />
         </a>
       </div>
       <div className="w-[40px] h-[40px] rounded-full m-1 border-solid border-slate-400">
@@ -18,16 +18,7 @@ const SocialMedia = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <BsGithub />
-        </a>
-      </div>
-      <div className="w-[40px] h-[40px] rounded-fulllm-1 border-solid border-slate-400">
-        <a
-          href="https://www.linkedin.com/in/aleksamarjanov/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BsLinkedin />
+          <BsFacebook />
         </a>
       </div>
     </div>
