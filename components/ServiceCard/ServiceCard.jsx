acd4dd9634224cb23c5ac4582620ccd4928ac19e
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-17 19:57:41 -0500

    Add responsive

--- components/ServiceCard/ServiceCard.jsx
+++ components/ServiceCard/ServiceCard.jsx
@@ -5,6 +5,7 @@ import { useTheme } from "next-themes";
 import { urlFor, client } from "../../pages/client";
 import {motion} from 'framer-motion'
 import { useRouter } from "next/router";
+import Link  from 'next/link';
 
 const ServiceCard = ({ isMobile }) => {
   const [services, setServices] = useState([]);
@@ -23,13 +24,13 @@ const ServiceCard = ({ isMobile }) => {
     <div className="flex justify-center items-center text-gray-600">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
-          <div className="w-full p-4 gap-8 grid grid-flow-col grid-rows-3">
+          <div className="p-16  gap-8 xl:gap-16 grid grid-flow-col grid-rows-3">
             {services.map((service, index) => (
               <div
                 key={service.name + index}
                 className={`flex flex-col ${
                   theme === "dark" ? "bg-nft-black-2" : "bg-nft-gray-2"
-                } items-center justify-center rounded-lg`}
+                } items-center justify-center rounded-lg sm:justify-between md:justify-between`}
               >
                 <Image
                   src={`${urlFor(service.imgurl)}`}
@@ -38,22 +39,23 @@ const ServiceCard = ({ isMobile }) => {
                   width={400}
                   height={400}
                   loading="lazy"
-                  className="object-center object-cover"
+                  className="object-center object-cover flex items-center justify-center"
                 />
                 <div
-                  className={`flex flex-col items-center justify-center p-6  text-white hover:text-nft-red-violet  transition duration-300 ease-in`}
+                  className={`xl:w-[50%] flex flex-col items-center justify-center p-6 text-white hover:text-nft-red-violet  transition duration-300 ease-in`}
                 >
                   <h1 className="text-2xl font-semibold mb-3">
                     {service.name}
                   </h1>
                   {/* <div className="w-[70%]"> */}
-                    <p className="leading-relaxed mb-3">
+                    <p className="leading-relaxed mb-3 md:text-2xl">
                       {service.description}
                     </p>
                   {/* </div> */}
                 </div>
+                <Link href='http://platinumbladesformen.booksy.com/'>
                 <motion.button
-                  className="box bottom-5 xs:m-2 sm:pt-2 sm:mb-5 rounded-full md:bottom-3 lg:bottom-3 nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white "
+                  className="box bottom-3 xs:m-2 sm:pt-2 sm:mb-5 rounded-full md:bottom-3 lg:bottom-3 nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white "
                   whileHover={{ scale: 1.1 }}
                   transition={{ type: "spring", stiffness: 400, damping: 10 }}
                   handleClick={() => router.push('http://platinumbladesformen.booksy.com/')}
@@ -61,6 +63,7 @@ const ServiceCard = ({ isMobile }) => {
                 Schedule an appointment
                 
                 </motion.button>
+                </Link>
               </div>
             ))}
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,7 @@ module.exports = {
       },
     },
     screens: {
+      xl: { max: '2300px'},
       lg: { max: '1800px' },
       md: { max: '990px' },
       sm: { max: '600px' },
