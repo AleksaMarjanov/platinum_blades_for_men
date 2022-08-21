2cdf83d904f86a81c0ba645474edcb1e8724c9b8
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-21 18:34:29 -0500

    editing map

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,25 +1,39 @@
-import React, {useState} from 'react'
-import ReactMapGL from 'react-map-gl'
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 const Map = () => {
-
-    const [viewport, setViewport] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8,
-    })
+  const [selectedLocation, setSelectedLocation] = useState({});
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 48.164926211456134,
+    longitude: -103.63274004145296,
+    zoom: 11,
+  });
 
   return (
     <ReactMapGL
-    mapStyle='mapbox://styles/aleksamarjanov/cl72iaroo001f14nusf0r0hwy'
-    mapboxAccessToken={process.env.mapbox_key}
-    {...viewport} 
+      mapStyle="mapbox://styles/aleksamarjanov/cl72iaroo001f14nusf0r0hwy"
+      mapboxAccessToken={process.env.mapbox_key}
+      viewport={viewport}
+      onViewPortChange={(nextViewPort) => setViewport(nextViewPort)}
     >
-
+      <Marker
+        longitude={-103.63274004145296}
+        latitude={48.164926211456134}
+        offsetLeft={-20}
+        offsetTop={-10}
+      >
+        <p role="img" className="cursor-pointer text-2xl animate-bounce" aria-label="push-pin">📌</p>
+      </Marker>
+      {/* <Popup
+    //   onClose={() => setSelectedLocation({})}
+      closeOnClick={true}
+      >
+            <p>Come and Check out the best barber</p>
+      </Popup> */}
     </ReactMapGL>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
