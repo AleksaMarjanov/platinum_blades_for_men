c62c8d5575d946f32dd318184c1a22d9f76415a7
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-08-25 18:50:57 -0500

    ..

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -12,7 +12,7 @@ const Map = () => {
       style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
-    url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWxla3NhbWFyamFub3YiLCJhIjoiY2wxNzg4OWdnNGNsdTNjcnB0eTUyaTFpZyJ9.Gb0b3LdcSTevZuB-w1ipCA"
+    url="https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWxla3NhbWFyamFub3YiLCJhIjoiY2wxNzg4OWdnNGNsdTNjcnB0eTUyaTFpZyJ9.Gb0b3LdcSTevZuB-w1ipCA"
     attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>"
   />
       <Marker position={[48.164714, -103.629075]} draggable={true} animate={true}>
